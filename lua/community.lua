8d6a629acbe8acd448f953f3b83636d04610a19b
Jim Riordan <jim@j1mr10rd4n.info>
2025-01-20 21:27:14 +1100

    Add typescript-all-in-one astrocommunity pack

--- lua/community.lua
+++ lua/community.lua
@@ -56,6 +56,7 @@ return {
   { import = "astrocommunity.colorscheme.vim-nightfly-colors" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
   { import = "astrocommunity.pack.clojure" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
   {
     --
     "AstroNvim/astrocore",
