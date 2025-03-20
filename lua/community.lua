ce590e8a01e12b4650a1b4e76b1640623bf91031
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-20 21:54:47 +1100

    community: Import colorscheme.everforest

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
