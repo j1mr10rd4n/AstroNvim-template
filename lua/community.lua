2ca83ed7d7b8ef5626e7254cbb03642ff9f147b1
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-21 19:57:10 +1100

    community: Import editor-support.vim-visual-multi plugin

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.color.ccc-nvim" },
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
