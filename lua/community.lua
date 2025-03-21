5447fb40f2b838ad120ca0b766349440382cac28
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-21 23:57:09 +1100

    community: Import git.octo-nvim plugin

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,7 @@ return {
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.git.octo-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
