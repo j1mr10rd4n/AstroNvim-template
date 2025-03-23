b0b8d1367a2e740f03e09f27a635ea340126aeb7
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-23 19:12:43 +1100

    community: Import recipes.heirline-nvchad-statusline

--- lua/community.lua
+++ lua/community.lua
@@ -15,6 +15,7 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.utility.noice-nvim" },
   -- import/override with your plugins folder
