92b9388ca1409771683061f3aa609f95365c6de2
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-23 15:31:46 +1100

    astrocore: Add next/prev buffer mappings (L/H)

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -59,6 +59,8 @@ return {
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
