c3c19dfffb4bea396c79ee21c661784c8ecea814
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-23 18:38:37 +1100

    noice: Add border to hover docs

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -1,5 +1,16 @@
 -- Override notification display defaults
 return {
+  {
+    "noice.nvim",
+    opts = function(_, opts)
+      local utils = require "astrocore"
+      return utils.extend_tbl(opts, {
+        presets = {
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+  },
   "rcarriga/nvim-notify",
   opts = {
     top_down = false,
