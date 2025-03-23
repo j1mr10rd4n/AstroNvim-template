44f56a8ad2a1a6116b5baed6f83badae216d39f4
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-23 19:43:17 +1100

    better-escape: Configure <fd> as <Esc> key mappings

--- lua/plugins/better-escape.nvim.lua
+++ lua/plugins/better-escape.nvim.lua
@@ -2,7 +2,14 @@
 return {
   "max397574/better-escape.nvim",
   opts = {
-    timeout = 300,
+    timeout = vim.o.timeoutlen,
     default_mappings = true,
+    mappings = {
+      i = { f = { d = "<Esc>" } },
+      c = { f = { d = "<Esc>" } },
+      t = { f = { d = "<Esc>" } },
+      v = { f = { d = "<Esc>" } },
+      s = { f = { d = "<Esc>" } },
+    },
   },
 }
