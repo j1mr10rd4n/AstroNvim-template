1842936d11bc9aee645232bb24c0a043a3087583
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-23 19:34:31 +1100

    lazy: Import trim plugin

---
+++ lua/plugins/trim.nvim.lua
@@ -0,0 +1,9 @@
+return {
+  "cappyzawa/trim.nvim",
+  opts = {
+    -- override default config
+    -- ft_blocklist = {"markdown"}, -- filetype not to trim
+    highlight = true,
+    highlight_bg = "#800080", -- or 'purple'
+  },
+}
