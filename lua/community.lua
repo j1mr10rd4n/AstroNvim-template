bb98599f587c46e4dae8e3cf625dac2bb012bf97
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-03-23 20:33:50 +1100

    community: Import gitlinker-nvim plugin

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,7 @@ return {
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.git.gitlinker-nvim" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.git.octo-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
