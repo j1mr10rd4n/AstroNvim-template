1125d33b9de659d513c5460453bd60acb68ed649
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-04-28 09:35:40 +1000

    Add community clojure pack

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
 
   { import = "astrocommunity.colorscheme.aurora" },
