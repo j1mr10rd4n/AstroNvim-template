4dda9002a0822b30f7bce4bb10ed9df857b509e4
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2025-05-01 15:27:36 +1000

    Add mini-align plugin

---
+++ lua/plugins/mini-align.lua
@@ -0,0 +1,8 @@
+return {
+  "echasnovski/mini.align",
+  version = "*",
+  opts = {
+    -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+    highlight_duration = 2000,
+  },
+}
