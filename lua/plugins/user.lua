49edb9e03228dbb73311dd088e0b35d127f16515
j1mr10rd4n <946427+j1mr10rd4n@users.noreply.github.com>
2026-08-22 15:41:50 +1000

    nvim: try waypoints plugin for bookmarks

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -32,6 +32,34 @@ return {
       },
     },
   },
+
+  -- bookmarks plugin - didn't work out of the box
+  -- {
+  --   "LintaoAmons/bookmarks.nvim",
+  --   -- pin the plugin at specific version for stability
+  --   -- backup your bookmark sqlite db when there are breaking changes (major version change)
+  --   tag = "v4.0.0",
+  --   dependencies = {
+  --     { "kkharji/sqlite.lua" },
+  --     -- picker backend (choose one):
+  --     { "folke/snacks.nvim" }, -- default picker backend
+  --     -- {"nvim-telescope/telescope.nvim"}, -- set picker.picker_backend = "telescope" to use
+  --   },
+  --   config = function()
+  --     local opts = {} -- check the "./lua/bookmarks/default-config.lua" file for all the options
+  --     require("bookmarks").setup(opts) -- you must call setup to init sqlite db
+  --   end,
+  -- },
+  --
+  {
+    "patrick-sharp/waypoint.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function() require("waypoint").setup {} end,
+  },
+
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
